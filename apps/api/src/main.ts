f87e0716a0297bed8f8efa48b107c5b409dd6c20
Versavvy Engineering <nfeleke568@gmail.com>
2023-08-25 15:55:42 +0300

    some changes

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,8 @@ const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 8080;
 
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
